b178feaec98a6bee4318f9a2ba065c9e1a958454
chaimae <chaimaeaafif@gmail.com>
2024-04-05 16:19:22 -0700

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is not None and len(a_dictionary) > 0:
+        max_score = max(a_dictionary.values())
+        for key, value in a_dictionary.items():
+            if value == max_score:
+                return key
+    return None
