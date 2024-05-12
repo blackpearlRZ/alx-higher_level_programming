3d4acf9f296f62353a5b7af7433867c9379243ef
chaimae <chaimaeaafif@gmail.com>
2024-05-12 06:31:38 -0700

    base

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''shbang'''
+
+
+class BaseGeometry:
+    '''geo class'''
+    def area(self):
+        '''error handlin'''
+        raise Exception("area() is not implemented")
