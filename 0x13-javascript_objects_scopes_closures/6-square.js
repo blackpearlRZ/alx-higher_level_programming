658ad0a8c27953e0d50c2be14b43e7a082111893
chaimae <chaimaeaafif@gmail.com>
2024-07-16 08:03:41 -0700

    object

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const Squar = require('./5-square');
+
+class Square extends Squar {
+  charPrint (c) {
+    if (typeof c === 'undefined') {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;#!/usr/bin/node
