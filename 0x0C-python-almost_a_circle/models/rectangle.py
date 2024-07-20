45b89fd4fc8c263f99608246ef09f18d3bd5f03c
chaimae <chaimaeaafif@gmail.com>
2024-07-20 06:26:36 -0700

    all

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -123,3 +123,15 @@ class Rectangle(Base):
     def area(self):
         '''returns rectangl earea'''
         return self.width * self.height
+
+    def display(self):
+        '''prints the Rectangle using the #'''
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
+
+        [print("") for y in range(self.y)]
+        for h in range(self.height):
+            [print(" ", end="") for x in range(self.x)]
+            [print("#", end="") for w in range(self.width)]
+            print("")
