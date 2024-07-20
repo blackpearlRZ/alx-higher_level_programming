edc80f5fba2af3771c6ccbd8707b061b6a203939
chaimae <chaimaeaafif@gmail.com>
2024-07-20 13:20:46 -0700

    sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Script that creates and setsup a db and a usea
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
