1a3197622a100fc973f4727ba031ed7cacbde3eb
chaimae <chaimaeaafif@gmail.com>
2024-07-20 06:23:22 -0700

    all

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -119,3 +119,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        '''returns rectangl earea'''
+        return self.width * self.height
