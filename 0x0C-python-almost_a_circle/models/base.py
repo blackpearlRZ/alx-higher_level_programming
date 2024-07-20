5d974d1b8e04a6f02bde27224a9a8b1cdd7424c9
chaimae <chaimaeaafif@gmail.com>
2024-07-20 05:25:28 -0700

    python

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 
 class Base:
-    '''creating base class'''
+    '''The base class for all other classes
+        It's purpose is to manage ID attribute
+        '''
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        ''''initilizing id'''
+        ''' Class constructor
+
+            Args:
+                id (int, optional): The id of the object
+        '''
         if id is not None:
             self.id = id
         else:
