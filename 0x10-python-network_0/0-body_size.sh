7598c9d741caffdd9c28f8b75c7fbfe1d2543b7d
chaimae <chaimaeaafif@gmail.com>
2024-08-04 14:52:35 -0700

    size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request to a URL and displays the body of the response
-curl -s -I "$1" | grep 'Content-Length' | cut -d ' ' -f 2
+#takes in a URL, sends a request to that URL
+curl -s "$1" | wc -c
